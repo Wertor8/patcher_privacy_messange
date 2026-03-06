e5885c8a052ae6f8b7e5192c600dc4b352a4c1bb
Wertor <nanipip169@gmail.com>
2026-03-06 20:32:01 +0300

    t2

---
+++ apk download/apk-to-git-master/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "packageName": "ru.oneme.app",
+    "targetFolder": "../source_apk",
+    "versionFile": "./local_version",
+    "git": {
+        "repository": "https://github.com/Wertor8/patcher_privacy_messange.git",
+        "credentials": {
+            "username": "username",
+            "password": "password",
+        },
+        "author": {
+            "name": "Bot",
+            "email": "bot@apktogit"
+        },
+        "commiter": {
+            "name": "Bot",
+            "email": "bot@apktogit"
+        }
+    }
+};
