9b88805e330f71eb218d3b428ef64da6cb8394f1
Wertor <nanipip169@gmail.com>
2026-03-19 20:12:48 +0300

    vf

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "packageName": "ru.oneme.app",
-    "targetFolder": "./patcher_privacy_messange",
+    "targetFolder": "./source_apk",
     "versionFile": "./source_apk",
     "git": {
         "repository": "https://github.com/Wertor8/patcher_privacy_messange.git",
