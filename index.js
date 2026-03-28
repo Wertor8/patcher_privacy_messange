2754b8e96b121be63dba52f87da0104f5b2fe1cb
Wertor <nanipip169@gmail.com>
2026-03-28 15:10:41 +0300

    2

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function download(downloadDir) {
     console.log("Navigating to " + url);
     await page.goto(url, {waitUntil: 'networkidle2'});
     
-    const downloadButtonSelector = '.variant.octs';
+    const downloadButtonSelector = '.file-list';
     console.log(`Waiting for initial download button with selector: ${downloadButtonSelector}`);
     await page.waitForSelector(downloadButtonSelector, { visible: true, timeout: 60000 });
     console.log("Initial download button found.");
